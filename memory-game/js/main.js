cf44723442c317447ff76d95a9224215efdf2f43
matthew wallace <matthewwallace@matthews-MacBook-Pro.local>
2020-02-16 20:23:27 -0600

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,34 @@
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+
+	
+
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
+
+
 const cardsInPlay = [];
 
 function checkForMatch() {
@@ -8,17 +37,21 @@ function checkForMatch() {
 } else {
   console.log("Sorry, try again.");
 }
-}
+};
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId])
-
+	console.log("User flipped " + cards[cardId].rank)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
+	cardsInPlay.push(cards[cardId].rank)
+	
 }
+	
+	flipCard(0);
+	flipCard(2);
+	
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+
 
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You have found a match");
